715f4844887f29332d32206e01d6abc7f9d3fa32
ValtheCoder <keshia.kdf@gmail.com>
2024-01-23 14:47:32 +0800

    Second Print

--- Evennumberletters.py
+++ Evennumberletters.py
@@ -1,5 +1,7 @@
 # Print "Original String is" + The word inputted by user
 word1 = input('Enter Word ')
 print("Original String is ", word1)
-# Print "Printing only even index chars
+# Print "Printing only even index chars"
+print("Printing only even index chars")
 # Set up the range and the even index characters only
+
