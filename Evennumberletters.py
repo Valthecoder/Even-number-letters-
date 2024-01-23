9b11b8ed79fb4ed1eb0c26fea4c137dd52ffecc6
ValtheCoder <keshia.kdf@gmail.com>
2024-01-23 14:50:40 +0800

    Listing and even index characters only

--- Evennumberletters.py
+++ Evennumberletters.py
@@ -4,4 +4,6 @@ print("Original String is ", word1)
 # Print "Printing only even index chars"
 print("Printing only even index chars")
 # Set up the range and the even index characters only
-
+a_word = list(word1)
+for word1 in a_word[0::2]:
+    print(word1)
